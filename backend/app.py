8f6ceb9896298114bfa6550b401782d15c23a4bf
Dana.D <dagherdana123@gmail.com>
2025-07-18 16:24:09 +0300

    docker_update2

--- backend/app.py
+++ backend/app.py
@@ -546,4 +546,4 @@ def home():
     return jsonify({"message": "Flask Backend is running!"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) # Run on port 5000
+    app.run(host='0.0.0.0',debug=True, port=5000) # Run on port 5000
